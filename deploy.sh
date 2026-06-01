bec5383720a4505b932e0b0389da7b5bdc6e116d
Harmee Patel <harmeepatel@gmail.com>
2026-06-01 17:29:14 -0400

    fix: back to old deploy

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -e
+
 # Build
 bun run build.js
+
 # Copy dist to a temp location outside the repo
 TMPDIR=$(mktemp -d)
 cp -r dist/* "$TMPDIR/"
 cp LICENSE "$TMPDIR/"
-echo "[www.wechitracreativehouse.in](https://www.wechitracreativehouse.in)" > "$TMPDIR/CNAME"
+echo "www.wechitracreativehouse.in" > "$TMPDIR/CNAME"
+
 CURRENT=$(git branch --show-current)
+
 # Switch to main and wipe it
 git checkout main
 git rm -rf .
+
 # Remove untracked leftovers (node_modules etc)
 rm -rf node_modules .gitignore dist build.js deploy.sh bun.lock package.json src out tmp web
+
 # Bring in the built files
-rsync -a --delete --exclude='.git' "$TMPDIR/" .
+cp -r "$TMPDIR"/* .
 rm -rf "$TMPDIR"
+
 # Commit and force push with single orphan commit
 git checkout --orphan temp
 git add .
@@ -23,5 +30,6 @@ git commit -m "deploy: $(date '+%Y-%m-%d %H:%M')"
 git branch -D main
 git branch -m main
 git push origin main --force
+
 git checkout "$CURRENT"
 echo "Deployed."
