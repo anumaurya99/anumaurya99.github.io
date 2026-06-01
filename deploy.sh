16b4eaa2ebbaa94fcd5a17ce538e7793823c28d4
Harmee Patel <harmeepatel@gmail.com>
2026-06-01 17:42:42 -0400

    fix: deploy and hero video

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,25 @@
 set -e
 # Build
 bun run build.js
+
 # Copy dist to a temp location outside the repo
 TMPDIR=$(mktemp -d)
 cp -r dist/* "$TMPDIR/"
 cp LICENSE "$TMPDIR/"
-echo "[www.wechitracreativehouse.in](https://www.wechitracreativehouse.in)" > "$TMPDIR/CNAME"
+echo "www.wechitracreativehouse.in" > "$TMPDIR/CNAME"
 CURRENT=$(git branch --show-current)
+
 # Switch to main and wipe it
 git checkout main
 git rm -rf .
+
 # Remove untracked leftovers (node_modules etc)
 rm -rf node_modules .gitignore dist build.js deploy.sh bun.lock package.json src out tmp web
+
 # Bring in the built files
 rsync -a --delete --exclude='.git' "$TMPDIR/" .
 rm -rf "$TMPDIR"
+
 # Commit and force push with single orphan commit
 git checkout --orphan temp
 git add .
