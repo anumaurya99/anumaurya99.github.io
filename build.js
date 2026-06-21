f13f36156d2776b1f81580c32d618f7e757361f9
Harmee Patel <harmeepatel@gmail.com>
2026-06-21 19:48:39 -0400

    feat: improving build script

--- build.js
+++ build.js
@@ -3,21 +3,35 @@ import { transform } from "lightningcss";
 import { readFileSync, writeFileSync, mkdirSync } from "fs";
 import { cpSync } from "fs";
 import { readdir } from "node:fs/promises";
+import path from "node:path";
 
 mkdirSync("./dist", { recursive: true });
 cpSync("./out/media", "./dist/media", { recursive: true });
 
-const outFiles = await readdir("./out", { recursive: true });
+const outFiles = (await readdir("./out", { recursive: true, withFileTypes: true }))
+    .filter((file) =>
+        !file.name.includes(".min.") &&
+        !file.name.endsWith(".webp") &&
+        !file.name.endsWith(".svg") &&
+        !file.name.endsWith(".mp4") &&
+        file.isFile()
+    )
+    .map((stat) => path.join(stat.path, stat.name))
 
+console.log(outFiles)
 // Minify JS
-const jsFiles = outFiles.filter((outFile) => outFile.includes(".js"));
+const jsFiles = outFiles.filter((outFile) => outFile.endsWith(".js"));
 if (jsFiles.length === 0) {
     throw new Error("No JS files found")
 }
 await Bun.build({
-    entrypoints: jsFiles.map((outFile) => "./out/" + outFile),
+    entrypoints: jsFiles.map((outFile) => outFile),
     outdir: "./dist",
-    minify: true,
+    minify: {
+        whitespace: true,
+        syntax: true,
+        identifiers: false,  // ← this is what causes the collision
+    },
 });
 
 // Minify CSS
@@ -26,7 +40,7 @@ if (cssFiles.length === 0) {
     throw new Error("No CSS files found")
 }
 for (const file of cssFiles) {
-    const css = readFileSync("./out/" + file);
+    const css = readFileSync(file);
     const { code } = transform({
         filename: file,
         code: css,
@@ -41,7 +55,7 @@ if (htmlFiles.length === 0) {
     throw new Error("No HTML files found")
 }
 for (const file of htmlFiles) {
-    const html = readFileSync("./out/" + file, "utf8");
+    const html = readFileSync(file, "utf8");
     const minified = await minify(html, {
         collapseWhitespace: true,
         removeComments: true,
