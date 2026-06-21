102d3286c1b77de3377dd8a08f50c86eb2741fc6
Harmee Patel <harmeepatel@gmail.com>
2026-06-21 19:51:03 -0400

    feat: improving build script

--- build.js
+++ build.js
@@ -3,7 +3,6 @@ import { transform } from "lightningcss";
 import { readFileSync, writeFileSync, mkdirSync } from "fs";
 import { cpSync } from "fs";
 import { readdir } from "node:fs/promises";
-import path from "node:path";
 
 mkdirSync("./dist", { recursive: true });
 cpSync("./out/media", "./dist/media", { recursive: true });
@@ -16,7 +15,7 @@ const outFiles = (await readdir("./out", { recursive: true, withFileTypes: true 
         !file.name.endsWith(".mp4") &&
         file.isFile()
     )
-    .map((stat) => path.join(stat.path, stat.name))
+    .map((stat) => stat.name)
 
 console.log(outFiles)
 // Minify JS
@@ -25,12 +24,12 @@ if (jsFiles.length === 0) {
     throw new Error("No JS files found")
 }
 await Bun.build({
-    entrypoints: jsFiles.map((outFile) => outFile),
+    entrypoints: jsFiles.map((outFile) => "./out/" + outFile),
     outdir: "./dist",
     minify: {
         whitespace: true,
         syntax: true,
-        identifiers: false,
+        identifiers: false,  // ← this is what causes the collision
     },
 });
 
@@ -40,7 +39,7 @@ if (cssFiles.length === 0) {
     throw new Error("No CSS files found")
 }
 for (const file of cssFiles) {
-    const css = readFileSync(file);
+    const css = readFileSync("./out/" + file);
     const { code } = transform({
         filename: file,
         code: css,
@@ -55,7 +54,7 @@ if (htmlFiles.length === 0) {
     throw new Error("No HTML files found")
 }
 for (const file of htmlFiles) {
-    const html = readFileSync(file, "utf8");
+    const html = readFileSync("./out/" + file, "utf8");
     const minified = await minify(html, {
         collapseWhitespace: true,
         removeComments: true,
